68438d5d81145f33e53c102b256a80317eb235f8
baekminsu <chodinggame@naver.com>
2023-01-21 08:21:13 +0900

    pyautotest

---
+++ pyautotest1.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+# 기본적인 payutotest1
+print(pyautogui.size())  #모니터 화면의 크기 출력
+time.sleep(2)
+print(pyautogui.position()) #마우스 포인터 위치 출력 1814,951 채팅창흰색 # 전송 1881,1018
+#pyautogui.click() 마우스클릭
